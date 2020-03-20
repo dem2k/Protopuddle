86e86ed626877f65ceafe2a637e709a18335685e
dem2k <dem@deminator.de>
2020-03-20 23:38:38 +0100

    refactoring.

--- src/main/java/defpackage/Sf.java
+++ src/main/java/defpackage/Sf.java
@@ -6,10 +6,11 @@ import java.awt.*;
  * @author DKL on 14.03.2020.
  */
 public class Sf {
-    public static final float factorx = 1.5f;
-    public static final Font font = new Font("Verdana", Font.BOLD, (int)(10*factorx));
-    public static final String resDir="/x1dot5";
-    public static int t(int val){
-        return (int)(val*factorx);
+    private static final float xfactr = 1.5f;
+    public static final String resDir = "/x1dot5";
+    public static final Font font = new Font("Verdana", Font.BOLD, (int) (10 * xfactr));
+
+    public static int t(int val) {
+        return (int) (val * xfactr);
     }
 }
