5e2e5cf006d8c187ea87708fd561123535da1791
dem2k <dem@deminator.de>
2020-03-21 01:09:00 +0100

    bigger font.

--- src/main/java/defpackage/Xf.java
+++ src/main/java/defpackage/Xf.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class Xf {
     private static final float xfactr = 1.5f;
     private static final String resDir = "/x1dot5";
-    public static final Font font = new Font("Verdana", Font.BOLD, (int) (10 * xfactr));
+    public static final Font font = new Font("Verdana", Font.BOLD, (int) (11 * xfactr));
 
     public static int up(int val) {
         return (int) (val * xfactr);
